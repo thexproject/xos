bd465b5432b3105a80689376fef84bf800446ee4
Felix Mattick <felix.mattick@gmail.com>
2018-11-08 10:43:28 -0500

    Google authentication

--- script.js
+++ script.js
@@ -26,7 +26,7 @@ Sentry.init({ dsn: "https://27c74054843742d5ad692d229d30c1bf@sentry.io/1318270" 
       this.height = height;
 
       code = xAct(code);
-      x("body").append(`
+      x("body").prepend(`
         <div class="xos-window" id="xos-window-${id}">
           <div class="xos-window-border">
             <span class="xos-window-name">${name}</span>
@@ -105,6 +105,16 @@ Sentry.init({ dsn: "https://27c74054843742d5ad692d229d30c1bf@sentry.io/1318270" 
       this.windowOffsetTop = 30;
       this.windowOffsetLeft = 30;
       this.windowReachedBottom = 0;
+
+      this.db = IronDB.IronDB
+    }
+
+    async doInitialization() {
+      this.bar.addToDOM();
+      await this.loadApplications();
+      this.startStartupApplications();
+      await this.loadBackgrounds();
+      this.setDefaultBackground();
     }
 
     async loadApplications() {
@@ -205,14 +215,14 @@ Sentry.init({ dsn: "https://27c74054843742d5ad692d229d30c1bf@sentry.io/1318270" 
         } else {
           x("html").style("background-image", `url("${this.backgrounds[id].value}")`);
         }
-        db.set("xos-default-wallpaper", id);
+        this.db.set("xos-default-wallpaper", id);
         logger.log(`Set background to ${id}.`);
       } else {
         logger.warn(`${id} isn't a loaded background.`);
       }
     }
     async setDefaultBackground() {
-      this.setBackground(await db.get("xos-default-wallpaper", Object.keys(this.backgrounds)[0]));
+      this.setBackground(await this.db.get("xos-default-wallpaper", Object.keys(this.backgrounds)[0]));
 
       logger.log("Set default background.");
     }
@@ -297,16 +307,25 @@ Sentry.init({ dsn: "https://27c74054843742d5ad692d229d30c1bf@sentry.io/1318270" 
 
   class xOSBar {
     constructor(menu) {
+      this.menu = menu;
+      this.open = {};
+    }
+
+    addToDOM() {
       this.element = x("body").prepend(`
         <div id="xos-bar">
           <i class="fas fa-compass" id="xos-menu-shower"></i>
           <span id="xos-bar-windows">
           </span>
+          <i class="fas fa-sign-out-alt" id="xos-logger-outer"></i>
         </div>
       `).find("#xos-bar-windows");
-      this.menu = menu;
       x("#xos-menu-shower").click(this.menu.toggle, this.menu);
-      this.open = {};
+      x("#xos-logger-outer").click(() => {
+        gapi.auth2.getAuthInstance().signOut().then(() => {
+          window.location.reload(false);
+        });
+      });
     }
 
     opened(window, application, name) {
@@ -439,17 +458,41 @@ Sentry.init({ dsn: "https://27c74054843742d5ad692d229d30c1bf@sentry.io/1318270" 
     }
   }
 
-  window.manager = new xOSManager("https://xcloud-heroku.herokuapp.com");
+  class xOSAuthenticator {
+    async start() {
+      x("body").prepend(`
+        <div id="xos-setup">
+          <h1>Let's get you set up</h1>
+          <div id="my-signin2"></div>
+        </div>
+      `);
+      gapi.signin2.render("my-signin2", {
+        "scope": "profile email openid",
+        "width": 240,
+        "height": 50,
+        "longtitle": true,
+        "theme": "dark",
+        "onsuccess": async user => {
+          x("#my-signin2").style("display", "none");
+          x("#xos-setup h1").destroy();
+          manager.doInitialization();
+        },
+        "onfailure": error => {
+          x("#my-signin2").destroy();
+          x("body").append(`
+            <p>You couldn't be logged in: ${error.error.replace(new RegExp("_", "g"), " ")}. <a href="javascript:window.location.reload(false);">Reload</a>.</p>
+          `);
+        }
+      });
+    }
+  }
+
   window.logger = new xOSLogger();
-  window.db = IronDB.IronDB;
+  window.manager = new xOSManager("https://xcloud-heroku.herokuapp.com");
+  window.authenticator = new xOSAuthenticator();
   let check = false;
   (a => {if(/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|mobile.+firefox|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows ce|xda|xiino|android|ipad|playbook|silk/i.test(a)||/1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(a.substr(0,4))) check = true;})(navigator.userAgent||navigator.vendor||window.opera);
   if (check) x("body").addClass("xos-is-mobile");
 })();
 
-(async () => {
-  await manager.loadApplications();
-  await manager.loadBackgrounds();
-  manager.setDefaultBackground();
-  manager.startStartupApplications();
-})();
+authenticator.start();
